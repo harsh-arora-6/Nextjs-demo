314357e8588edd351d92df2c10d1d520a5facec3
Harsh Arora <arora99harsh@gmail.com>
2023-06-27 10:21:43 +0530

    The _app.js file and layout wrapper

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eb33a5865fec4996b578";
+/******/ 	var hotCurrentHash = "be331d1e97788da12b8f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
