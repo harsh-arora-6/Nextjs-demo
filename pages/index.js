438929f4d74e88e54b016e67d0791d10ec6e1a8b
Harsh Arora <arora99harsh@gmail.com>
2023-06-27 10:03:20 +0530

    Outputting list of meetups

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import MeetupList from "../components/meetups/MeetupList";
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBYm7g_W9BtaVvrBtn1PAp1oZ59vHHfuIKOlHvXhgi&s",
+    title: "First Meetup",
+    address: "random address",
+  },
+  {
+    id: "m2",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBYm7g_W9BtaVvrBtn1PAp1oZ59vHHfuIKOlHvXhgi&s",
+    title: "Second Meetup",
+    address: "random address",
+  },
+];
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
